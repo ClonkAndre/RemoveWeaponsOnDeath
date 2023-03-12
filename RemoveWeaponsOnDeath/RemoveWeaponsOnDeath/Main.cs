0b4d927717134efa8da5d73e9dce79d73a353248
Elias <napaln@gmx.de>
2023-03-13 00:36:31 +0100

    Added vs project

---
+++ RemoveWeaponsOnDeath/RemoveWeaponsOnDeath/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using IVSDKDotNet;
+using static IVSDKDotNet.Native.Natives;
+
+namespace RemoveWeaponsOnDeath {
+    public class Main : Script {
+
+        #region Constructor
+        public Main()
+        {
+            Tick += Main_Tick;
+        }
+        #endregion
+
+        private void Main_Tick(object sender, EventArgs e)
+        {
+            int playerID = CONVERT_INT_TO_PLAYERINDEX(GET_PLAYER_ID());
+            GET_PLAYER_CHAR(playerID, out int playerPed);
+
+            if (IS_CHAR_DEAD(playerPed)) REMOVE_ALL_CHAR_WEAPONS(playerPed);
+        }
+
+    }
+}
